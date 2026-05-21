756854e3b974a3ce88f48277a9fbbcc0f655dc99
Sparrowsyntax3210 <Sparrowsyntax3210@gmail.com>
2026-05-21 18:06:35 +0530

    About changes

---
+++ Harsh Goel - Personal portfolio/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/root", (req, res) => {
+  res.send("this is root");
+});
+
+module.exports = app;
